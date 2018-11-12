3f6eb6779156f80fe88b70628ecba75f1c02492d
isaacvonderau <isaacvonderau@gmail.com>
2018-11-11 18:03:49 -0800

    working script for sort function

---
+++ js/sortList.js
@@ -0,0 +1,55 @@
+
+/*Alphabetical Sort*/
+var list, i, switching, b, shouldSwitch, c;
+list =document.getElementById("publications");
+b = list.getElementsByTagName("LI");
+
+
+function sortList() 
+{
+    switching = true;
+    while(switching)
+    {
+    switching=false;
+    for ( i = 0  ; i < (b.length -1); i++)
+        {
+            shouldSwitch = false;
+            if (b[i].innerHTML.toLocaleLowerCase() > b[ i + 1 ].innerHTML.toLocaleLowerCase())
+            {
+                shouldSwitch = true;
+                break;
+            }
+        }
+    
+    if(shouldSwitch)
+        {
+                b[i].parentNode.insertBefore(b[i+1], b[i]);
+                switching = true;
+        }
+    }
+};
+
+/*Sort by most recent*/
+function sortYear()
+{
+    switching = true;
+    while(switching)
+    {
+        switching = false;
+        for (i = 0 ; i < (b.length -1); i++)
+        {
+            shouldSwitch = false;
+            if (b[i].getAttribute('id') < b[i + 1].getAttribute('id'))
+            {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if(shouldSwitch)
+        {
+                b[i].parentNode.insertBefore(b[i+1], b[i]);
+                switching = true;
+        }
+
+    }
+};
